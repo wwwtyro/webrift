7b8a5b30ea2f6eca3d332167f8f6792eeb6f4dd0
rye terrell <ryterrell@ryeterrell.net>
2013-12-13 07:26:23 -0600

    hub

---
+++ hub/demos.js
@@ -0,0 +1,22 @@
+var demos = [
+	{	
+		"name": "Conway 3D",
+		"url": "http://wwwtyro.github.io/webrift/demos/conway3d",
+		"screenshot": "conway3d.png"	
+	},
+	{   
+		"name": "Astray",
+		"url": "http://wwwtyro.github.io/webrift/demos/astray",
+		"screenshot": "astray.png"	
+	},
+	{	
+		"name": "Hello World",
+		"url": "http://wwwtyro.github.io/webrift/demos/helloworld",
+		"screenshot": "helloworld.png"	
+	},
+	{
+		"name": "Oculus Street View",
+		"url": "http://oculusstreetview.eu.pn/",
+		"screenshot": "osv.png"
+	},
+];
